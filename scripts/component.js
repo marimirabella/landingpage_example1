ffd389b2c1ea5b21c384674cb87b8a446fcb8291
Marina <marina4ya@gmail.com>
2016-09-13 20:41:40 +0300

    first commit

---
+++ scripts/component.js
@@ -0,0 +1,23 @@
+﻿function Component(){
+	this.init = function(sSelector){
+		/* Свойство elem - общее для вех объектов, которые будут наследоваться от component. идентификатор роль */
+		this.elem = $(sSelector);
+	//Ошибка доступа к главному селектору
+	if(!this.elem.length){  //!this - not this
+		alert("Can't caccess element by selector: " + sSelector);
+		}	
+	}
+	this.find = function(sSelector){ //роль класса
+		var findResult = this.elem.find(sSelector);
+		if(findResult.length){
+			return findResult;
+		}
+		else{
+			alert("Could not find element by selector " + sSelector);
+		}
+	}
+	
+}
+	
+	
+	
